5471f17bbcd8f3cff90d5865acd5ee8525b52513
vladislavkotx <44949371+vladislavkotx@users.noreply.github.com>
2021-07-28 08:14:45 +0100

    Update string_calculator_test.py

--- string_calculator_test.py
+++ string_calculator_test.py
@@ -1,3 +1,49 @@
-import string_calculator.py
+import re
 
-parse("sff,\\ * fsfd")
+
+class StringCalculator:
+    def __init__(self):
+        self.tokens = []
+
+    def parse(self, exp):
+        regex = ",\\ *"
+
+        tempResult = re.split(regex, exp)
+
+        if len(tempResult) > 0:
+            for tag in tempResult:
+                self.tokens.append(tag)
+
+            result = self.tokenise(self.tokens)
+
+        return self.tokens
+
+    def tokenise(self, tokens):
+        result = re.findall("\+|\-|\*|/|%", tokens[0])
+        if len(result) > 0:
+            self.tokens[0] = "OPERATOR"
+        else:
+            self.tokens[0] = "INVALID_OPERATOR"
+
+            result = tokens[1].isdigit()
+            if result:
+                self.tokens[1] = "NUMBER"
+            else:
+                self.tokens[1] = "INVALID_NUMBER"
+
+            result = tokens[2].isdigit()
+            if result:
+                self.tokens[2] = "NUMBER"
+            else:
+                self.tokens[2] = "INVALID_NUMBER"
+
+            return result
+
+str = StringCalculator()
+my_str = '123*123'
+# my_str2 = '123'
+str.parse(my_str)
+# str.parse(my_str2)
+print(str.tokens)
+
+# regex = ",\\ *"
